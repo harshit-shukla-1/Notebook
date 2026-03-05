40f0b8430938f7f17fdaca2c2508388fea1e1280
[dyad] <shuklaharshit0101@gmail.com>
2026-03-06 01:56:27 +0530

    Update 1 file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9t0mrgqp39g"
+    "revision": "0.pgqcsk81dt8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
